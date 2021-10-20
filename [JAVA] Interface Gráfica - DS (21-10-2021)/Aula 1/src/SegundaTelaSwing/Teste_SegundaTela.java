2074bb2e5ea3c309468787103d83d314eb7e4fbc
Felipe Jacinto <74006952+DS-Felipe-Jacinto@users.noreply.github.com>
2021-10-20 17:14:06 -0300

    Add files via upload

---
+++ [JAVA] Interface Gráfica - DS (21-10-2021)/Aula 1/src/SegundaTelaSwing/Teste_SegundaTela.java
@@ -0,0 +1,10 @@
+package SegundaTelaSwing;
+
+public class Teste_SegundaTela {
+	public static void main(String[]args){
+		
+		// Chamando o construtor da Tela 2
+		// O Construtor com o tamanho da tela (sendo eles: (X,Y))
+		SegundaTelaSwing Tela2 = new SegundaTelaSwing();
+	}
+}
