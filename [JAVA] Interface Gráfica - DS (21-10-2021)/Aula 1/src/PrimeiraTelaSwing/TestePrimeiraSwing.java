d048023f0625ac7d0af7d73a0d59e455b008305b
Felipe Jacinto <74006952+DS-Felipe-Jacinto@users.noreply.github.com>
2021-10-20 17:12:20 -0300

    Delete [JAVA] Interface Gráfica - DS (21-10-2021) directory

--- [JAVA] Interface Gráfica - DS (21-10-2021)/Aula 1/src/PrimeiraTelaSwing/TestePrimeiraSwing.java
+++
@@ -1,8 +0,0 @@
-package PrimeiraTelaSwing;
-	public class TestePrimeiraSwing{
-		public static void main(String[]args) {
-			// Chama o construtor da tela
-			// O construtor com o tamanho da tela(X,Y)
-			PrimeiraTelaSwing tela = new PrimeiraTelaSwing();
-	}
-}
